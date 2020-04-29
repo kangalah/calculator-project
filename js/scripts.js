151d90fa20f423d684e987ec26825108cbb3e94f
joan <nyongesajoannanjala@gmail.com>
2020-04-29 22:25:28 +0300

    added division function

--- js/scripts.js
+++ js/scripts.js
@@ -21,3 +21,12 @@ var add = function(number1, number2){
             var number2 = parseInt(prompt("Enter a number:"));
             var result = multiply(number1,number2);
             alert(result);
+
+            var division = function(number1, number2){
+                return number1 / number2;
+                };
+                var number1 = parseInt(prompt("Enter a number:"));
+                var number2 = parseInt(prompt("Enter a number:"));
+                var result = division(number1,number2);
+                alert(result);
+                
